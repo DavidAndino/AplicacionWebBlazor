6090cddb526d074ea4a9b59ecc5a123babba82d1
DavidAndino <123537730+DavidAndino@users.noreply.github.com>
2023-04-12 11:42:58 -0600

    avanzando

---
+++ Web/Blazor/Controllers/LoginController.cs
@@ -0,0 +1,88 @@
+﻿using Datos.Interface;
+using Datos.Repositorios;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Modelos;
+using System.Security.Claims;
+
+namespace Blazor.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly Config _config;
+        private ILoginRepositorio _loginRepositorio;
+        private IUsuarioRepositorio _usuarioRepositorio;
+
+        LoginController(Config config)
+        {
+            _config = config;
+            _loginRepositorio = new LoginRepositorio(config.CadenaConexion);
+            _usuarioRepositorio = new UsuarioRepositorio(config.CadenaConexion);
+        }
+
+        //metodo para autenticar y abrir sesion
+        [HttpPost("/autenticar/validar")]
+        public async Task<IActionResult> validacion(Login login)
+        {
+            string rol = string.Empty;//almacenando rol temporalmente. Inicialmente la variable estara vacia
+            try
+            {
+                bool usuarioValido = await _loginRepositorio.validacionUsuarioAsync(login);//validando que el user ingrese contrasenia y usuario
+
+                //validando si el user esta activo
+                if (usuarioValido)
+                {
+                    //se consume un metodo de UsuarioRepositorio que permita devolver el usuario. Se consulta a todo el objeto "usuario", para validar el estado y rol tambien
+                    Usuario user = await _usuarioRepositorio.TraerPorCodigoAsync(login.UserCode);
+
+                    if (user.Active)
+                    {
+                        rol = user.Role;
+
+                        //creando sesion
+                        var claims = new[]
+                        {
+                            new Claim(ClaimTypes.Name, user.UserCode),//tipo de Claim: un Claim para el codigo usuario y otro para el rol
+                            new Claim(ClaimTypes.Role, user.Role)
+                        };
+
+                        ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);/*pasando el arreglo de "Claims" anterior y tipo de autenticacion
+                                                                                  cookies, significa que se utiliza el navegador para auntenticar
+                                                                                    en una "cookie" se guardara la sesion (no es la mas segura). al claims identity se le pasan los valores
+                                                                                    */
+                        ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(claimsIdentity);//este ejecutara la sesion
+                        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal, new AuthenticationProperties { IsPersistent = true, ExpiresUtc = DateTime.UtcNow.AddMinutes(20) });
+                        /*Permite acceder al contexto del sitio o pagina web
+                         la propiedad (ultimo parametro) es para especificar 
+                         cuanto tiempo durara la sesion
+                        */
+                    }
+                    else
+                    {
+                        //mandando a una ruta, en esta caso a la ruta /login, porque no esta activo el user y no se pudo iniciar sesion
+                        return LocalRedirect("/login/El usuario no está activo");
+                    }
+                }
+                else
+                {
+                    return LocalRedirect("/login/Datos ingresados inválidos");
+                }
+
+            }
+            catch (Exception)
+            {
+            }
+            return LocalRedirect("/");
+        }
+
+        //metodo para cerrar la sesion
+        [HttpGet("/salir")]
+        public async Task<IActionResult> cierreSesion()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return LocalRedirect("/login");
+        }
+
+    }
+}
--- Web/Blazor/Program.cs
+++ Web/Blazor/Program.cs
@@ -1,6 +1,7 @@
 using Blazor;
 using Blazor.Interfaces;
 using Blazor.Servicios;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +16,8 @@ builder.Services.AddSingleton(Cadena);//configurando servicio: ahora se puede ut
 //configurando servicios de la clase "UsuarioServicio"
 builder.Services.AddScoped<ILoginServicio, LoginServicio>();//se le pasa la interfaz y la clase del servicio como parametros
 builder.Services.AddScoped<IUsuarioServicio, UsuarioServicio>();
-
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie();//pansando el servicio de tipo de autenticacion
+builder.Services.AddHttpContextAccessor();//sirve para poder acceder a los datos del  usuario que inicio sesion
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -29,9 +31,12 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
-
 app.UseRouting();
 
+app.UseAuthentication();//indicando que se maneja autenticacion: que exista el user en la dB
+app.UseAuthorization();//indicando que se maneja autorizacion: tipos de roles del  usuario
+
+app.MapControllers();//indicando que la app de  Blazor utilizara controladores; sino tira error.
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
